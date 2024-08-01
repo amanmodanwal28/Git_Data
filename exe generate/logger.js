06b2a5572b7ace33f45cc5b45ed2d980bf614e2e
Aman Modanwal <itisaman28@gmail.com>
2024-08-01 18:07:29 +0530

    second to first

---
+++ exe generate/logger.js
@@ -0,0 +1,74 @@
+const { app } = require('electron')
+const { createLogger, format, transports } = require('winston')
+const path = require('path')
+const os = require('os')
+const fs = require('fs')
+
+// Get the path to the installation directory
+const installationPath = path.dirname(process.execPath)
+
+// Use a user-writable directory for logs
+const logsPath = path.join(os.homedir(), 'AppData', 'Local', 'PT-Tool', 'logs')
+
+// Helper function to ensure the logs directory exists and handle errors
+const ensureDirectoryExists = (dirPath) => {
+    try {
+        if (!fs.existsSync(dirPath)) {
+            fs.mkdirSync(dirPath, { recursive: true })
+        }
+    } catch (error) {
+        // Log the error to a fallback log file
+        const fallbackLogFile = path.join(os.homedir(), 'Downloads', 'log.txt')
+        fs.appendFileSync(
+            fallbackLogFile,
+            `Failed to create directory ${dirPath}: ${error}\n`
+        )
+        console.error(`Failed to create directory ${dirPath}: ${error}`)
+        return false
+    }
+    return true
+}
+
+// Check if the logs directory exists and handle accordingly
+const directoryExists = ensureDirectoryExists(logsPath)
+
+const logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        format.errors({ stack: true }),
+        format.splat(),
+        format.json()
+    ),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new transports.File({
+            filename: path.join(logsPath, 'error.log'),
+            level: 'error',
+            handleExceptions: true
+        }),
+        new transports.File({
+            filename: path.join(logsPath, 'combined.log'),
+            handleExceptions: true
+        })
+    ]
+})
+
+// Fallback logging if the directory is not accessible
+if (!directoryExists) {
+    logger.transports.forEach((t) => {
+        if (t instanceof transports.File) {
+            t.filename = path.join(os.homedir(), 'Downloads', 'log.txt')
+        }
+    })
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(
+        new transports.Console({
+            format: format.combine(format.colorize(), format.simple())
+        })
+    )
+}
+
+module.exports = logger
---
+++ exe generate/xmlParser.js
@@ -0,0 +1,200 @@
+const { app } = require('electron');
+const fs = require('fs')
+const path = require('path')
+const xml2js = require('xml2js')
+const util = require('util')
+const logger = require('./logger')
+let installationPath = path.dirname(process.execPath)
+    // Path to config.xml
+let xmlFilePath = path.join(installationPath, 'resources', 'Iot_Config.xml') // Adjust the file path as needed
+
+// Promisify fs functions
+const fsAccess = util.promisify(fs.access)
+const fsReadFile = util.promisify(fs.readFile)
+const fsWriteFile = util.promisify(fs.writeFile)
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///    for create  the directory and   pass the data in main.js   ////////////////////////////////////////////////////
+
+// Define the base path where the directories should be created
+let basePath = path.join(__dirname, 'content')
+
+// Arrays to store folder names
+const folderNames = []
+const subFolderNames = {}
+const subSubFolderNames = {}
+
+const createDirectories = (dirPath) => {
+    // console.log(typeof dirPath)
+    if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, { recursive: true })
+            // console.log(`Directory created: ${dirPath}`);
+            // logger.info(`Directory created: ${dirPath}`)
+    } else {
+        // logger.info(`Directory already exists: ${dirPath}`)
+        // console.log(`Directory already exists: ${dirPath}`);
+    }
+}
+
+const parseXMLAndCreateDirectories = (xml) => {
+    return new Promise((resolve, reject) => {
+        xml2js.parseString(xml, (err, result) => {
+            if (err) {
+                logger.error(`Error parsing XML:, ${err}`)
+                reject(err)
+                return
+            }
+
+            const dataStructure = result.IOTConfig.DATA_STRUCTURE[0]
+
+            for (const key in dataStructure) {
+                if (Array.isArray(dataStructure[key])) {
+                    dataStructure[key].forEach((element) => {
+                        const folderName = element['$'].folder_name
+                        folderNames.push(folderName)
+                        const folderUrl = path.join(basePath, folderName)
+                        createDirectories(folderUrl)
+
+                        if (!subFolderNames[folderName]) {
+                            subFolderNames[folderName] = []
+                        }
+
+                        for (const subKey in element) {
+                            if (Array.isArray(element[subKey])) {
+                                element[subKey].forEach((subElement) => {
+                                    const subFolderName =
+                                        subElement['$'][Object.keys(subElement['$'])[0]]
+                                    subFolderNames[folderName].push(subFolderName)
+                                    const subFolderUrl = path.join(folderUrl, subFolderName)
+                                    createDirectories(subFolderUrl)
+
+                                    if (!subSubFolderNames[subFolderName]) {
+                                        subSubFolderNames[subFolderName] = []
+                                    }
+
+                                    for (const subSubKey in subElement) {
+                                        if (Array.isArray(subElement[subSubKey])) {
+                                            subElement[subSubKey].forEach((subSubElement) => {
+                                                const subSubFolderName =
+                                                    subSubElement['$'][Object.keys(subSubElement['$'])[0]]
+                                                subSubFolderNames[subFolderName].push(subSubFolderName)
+                                                const subSubFolderUrl = path.join(
+                                                    subFolderUrl,
+                                                    subSubFolderName
+                                                )
+                                                createDirectories(subSubFolderUrl)
+                                            })
+                                        }
+                                    }
+                                })
+                            }
+                        }
+                    })
+                }
+            }
+
+            resolve()
+        })
+    })
+}
+
+const createDatabaseDirectories = (CreateMainFolder) => {
+    basePath = CreateMainFolder
+
+    return new Promise((resolve, reject) => {
+        fs.readFile(xmlFilePath, 'utf8', async(err, xmlData) => {
+            if (err) {
+                logger.error(`Error reading XML file:, ${err}`)
+                console.log(`Error reading XML file: ${err}`)
+                reject(err)
+                return
+            }
+            // Check if the main "content" directory exists
+            try {
+                await fsAccess(basePath)
+                logger.info(`Directory already exists: %s, ${basePath}`)
+                console.log(`Directory already exists: ${basePath}`)
+            } catch {
+                logger.error(`Error creating directories:, ${err}`)
+                console.log(`Main directory does not exist: ${basePath}`)
+                throw new Error(`Main directory does not exist: ${basePath}`)
+            }
+
+            // Create the INDEX.SYS file
+            const indexSysPath = path.join(basePath, 'INDEX.SYS')
+            await fsWriteFile(indexSysPath, '') // Adjust content as needed
+            logger.info(`INDEX.SYS file created at: %s, {$indexSysPath}`)
+            console.log(`INDEX.SYS file created at: ${indexSysPath}`)
+                // Parse XML and create directories
+            parseXMLAndCreateDirectories(xmlData).then(resolve).catch(reject)
+        })
+    })
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///    for Reading the xml data and  pass the data in main.js   ////////////////////////////////////////////////////
+
+async function readXmlFile(filePath) {
+    xmlFilePath = filePath
+    logger.info(`xml path found at : ${filePath}`)
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                logger.error(`Error reading XML file:, ${err}`)
+
+                reject(err)
+                return
+            }
+            resolve(data)
+        })
+    })
+}
+
+function extractFolderInfo(dataStructure, result = []) {
+    for (const key in dataStructure) {
+        const value = dataStructure[key]
+        if (typeof value === 'object' && value !== null) {
+            if (value.$) {
+                const folderInfo = { tag: key }
+                for (const attr in value.$) {
+                    folderInfo[attr] = value.$[attr]
+                }
+                result.push(folderInfo)
+            }
+            // Recursively check nested objects
+            extractFolderInfo(value, result)
+        }
+    }
+    return result
+}
+
+function extractField(folderInfo, fieldName) {
+    return folderInfo
+        .map((content) => content[fieldName])
+        .filter((name) => name !== undefined)
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+module.exports = {
+    createDatabaseDirectories,
+    readXmlFile,
+    extractFolderInfo,
+    extractField,
+    folderNames,
+    subFolderNames,
+    subSubFolderNames
+}
